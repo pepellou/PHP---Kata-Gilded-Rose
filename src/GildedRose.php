0e6bc19739b36fc137e4992334fbc48c6e16edb6
Pepe Doval <pepellou@gmail.com>
2011-11-04 17:43:37 +0100

    Extracted methods for updating sellIn and Quality

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,26 +4,40 @@ require_once 'src/ItemGildedRose.php';
 
 class GildedRose {
 
-	public static function updateQuality(
-		$items
+	private static function updateSellIn(
+		$item
 	) {
-		for ($i = 0; $i < count($items); $i++) {
-			$item = new ItemGildedRose($items[$i]);
+		if (!$item->isSulfuras()) {
+			$item->decreaseSellIn();
+		}
+	}
 
-			if (!$item->isSulfuras()) {
-				$item->decreaseSellIn();
+	private static function updateQualityOfItem(
+		$item
+	) {
+		if ($item->isAgedBrie()) {
+			$item->increaseQuality();
+		} else if ($item->isBackstagePass()) {
+			$item->increaseQuality();
+			if ($item->isCloseToExpire()) {
+				$item->increaseQuality();
 			}
+			if ($item->isVeryCloseToExpire()) {
+				$item->increaseQuality();
+			}
+		} else if ($item->isSulfuras()) {
+		} else if ($item->isConjured()) {
+			$item->decreaseQuality();
+			$item->decreaseQuality();
+		} else {
+			$item->decreaseQuality();
+		}
 
+		if ($item->isExpired()) {
 			if ($item->isAgedBrie()) {
 				$item->increaseQuality();
 			} else if ($item->isBackstagePass()) {
-				$item->increaseQuality();
-				if ($item->isCloseToExpire()) {
-					$item->increaseQuality();
-				}
-				if ($item->isVeryCloseToExpire()) {
-					$item->increaseQuality();
-				}
+					$item->setMinimumQuality();
 			} else if ($item->isSulfuras()) {
 			} else if ($item->isConjured()) {
 				$item->decreaseQuality();
@@ -31,20 +45,17 @@ class GildedRose {
 			} else {
 				$item->decreaseQuality();
 			}
+		}
+	}
 
-			if ($item->isExpired()) {
-				if ($item->isAgedBrie()) {
-					$item->increaseQuality();
-				} else if ($item->isBackstagePass()) {
-						$item->setMinimumQuality();
-				} else if ($item->isSulfuras()) {
-				} else if ($item->isConjured()) {
-					$item->decreaseQuality();
-					$item->decreaseQuality();
-				} else {
-					$item->decreaseQuality();
-				}
-			}
+	public static function updateQuality(
+		$items
+	) {
+		for ($i = 0; $i < count($items); $i++) {
+			$item = new ItemGildedRose($items[$i]);
+
+			self::updateSellIn($item);
+			self::updateQualityOfItem($item);
 		}
 	}
 }

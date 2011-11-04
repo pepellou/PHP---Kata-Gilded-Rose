10ab99606a1d55be147a4790cbdf02a7891b311e
Pepe Doval <pepellou@gmail.com>
2011-11-04 18:11:08 +0100

    Inline methods

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -9,56 +9,25 @@ require_once 'src/SulfurasUpdater.php';
 
 class GildedRose {
 
-	private static function updateSellIn(
-		$item
-	) {
-		if ($item->isAgedBrie()) {
-			$updater = new AgedBrieUpdater();
-			$updater->updateSellIn($item);
-		} else if ($item->isBackstagePass()) {
-			$updater = new BackstagePassUpdater();
-			$updater->updateSellIn($item);
-		} else if ($item->isSulfuras()) {
-			$updater = new SulfurasUpdater();
-			$updater->updateSellIn($item);
-		} else if ($item->isConjured()) {
-			$updater = new ConjuredUpdater();
-			$updater->updateSellIn($item);
-		} else {
-			$updater = new ItemUpdater();
-			$updater->updateSellIn($item);
-		}
-	}
-
-	private static function updateQualityOfItem(
-		$item
-	) {
-		if ($item->isAgedBrie()) {
-			$updater = new AgedBrieUpdater();
-			$updater->updateQuality($item);
-		} else if ($item->isBackstagePass()) {
-			$updater = new BackstagePassUpdater();
-			$updater->updateQuality($item);
-		} else if ($item->isSulfuras()) {
-			$updater = new SulfurasUpdater();
-			$updater->updateQuality($item);
-		} else if ($item->isConjured()) {
-			$updater = new ConjuredUpdater();
-			$updater->updateQuality($item);
-		} else {
-			$updater = new ItemUpdater();
-			$updater->updateQuality($item);
-		}
-	}
-
 	public static function updateQuality(
 		$items
 	) {
 		for ($i = 0; $i < count($items); $i++) {
 			$item = new ItemGildedRose($items[$i]);
 
-			self::updateSellIn($item);
-			self::updateQualityOfItem($item);
+			if ($item->isAgedBrie()) {
+				$updater = new AgedBrieUpdater();
+			} else if ($item->isBackstagePass()) {
+				$updater = new BackstagePassUpdater();
+			} else if ($item->isSulfuras()) {
+				$updater = new SulfurasUpdater();
+			} else if ($item->isConjured()) {
+				$updater = new ConjuredUpdater();
+			} else {
+				$updater = new ItemUpdater();
+			}
+			$updater->updateSellIn($item);
+			$updater->updateQuality($item);
 		}
 	}
 }

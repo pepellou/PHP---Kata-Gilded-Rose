d99cb810385f13504951cd066f3f3ace01a86cd0
Pepe Doval <pepellou@gmail.com>
2011-11-04 20:42:24 +0100

    Simplified inner use of getters/setters

--- src/ItemGildedRose.php
+++ src/ItemGildedRose.php
@@ -20,46 +20,46 @@ class ItemGildedRose extends Item {
 
 	public function decreaseQuality(
 	) {
-		$this->setQuality($this->getQuality() - 1);
-		if ($this->getQuality() < self::MINIMUM_QUALITY)
+		$this->quality--;
+		if ($this->quality < self::MINIMUM_QUALITY)
 			$this->setMinimumQuality();
 	}
 
 	public function increaseQuality(
 	) {
-		$this->setQuality($this->getQuality() + 1);
-		if ($this->getQuality() > self::MAXIMUM_QUALITY)
+		$this->quality++;
+		if ($this->quality > self::MAXIMUM_QUALITY)
 			$this->setMaximumQuality();
 	}
 
 	public function decreaseSellIn(
 	) {
-		$this->setSellIn($this->getSellIn() - 1);
+		$this->sellIn--;
 	}
 
 	public function setMinimumQuality(
 	) {
-		$this->setQuality(self::MINIMUM_QUALITY);
+		$this->quality = self::MINIMUM_QUALITY;
 	}
 
 	public function setMaximumQuality(
 	) {
-		$this->setQuality(self::MAXIMUM_QUALITY);
+		$this->quality = self::MAXIMUM_QUALITY;
 	}
 
 	public function isExpired(
 	) {
-		return $this->getSellIn() < 0;
+		return $this->sellIn < 0;
 	}
 
 	public function isCloseToExpire(
 	) {
-		return $this->getSellIn() < 10;
+		return $this->sellIn < 10;
 	}
 
 	public function isVeryCloseToExpire(
 	) {
-		return $this->getSellIn() < 5;
+		return $this->sellIn < 5;
 	}
 
 }

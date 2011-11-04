234099c9629a27287c8efc349c23d6dc495d311f
Pepe Doval <pepellou@gmail.com>
2011-11-04 15:23:56 +0100

    Extracted methods to determine type of item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,21 +4,39 @@ require_once 'src/Item.php';
 
 class GildedRose {
 
+	private static function isAgedBrie(
+		$item
+	) {
+		return "Aged Brie" == $item->getName();
+	}
+
+	private static function isBackstagePass(
+		$item
+	) {
+		return "Backstage passes to a TAFKAL80ETC concert" == $item->getName();
+	}
+
+	private static function isSulfuras(
+		$item
+	) {
+		return "Sulfuras, Hand of Ragnaros" == $item->getName();
+	}
+
 	public static function updateQuality(
 		$items
 	) {
 		for ($i = 0; $i < count($items); $i++) {
 			$item = $items[$i];
-			if (("Aged Brie" != $item->getName()) && ("Backstage passes to a TAFKAL80ETC concert" != $item->getName())) {
+			if (!self::isAgedBrie($item) && !self::isBackstagePass($item)) {
 				if ($item->getQuality() > 0) {
-					if ("Sulfuras, Hand of Ragnaros" != $item->getName()) {
+					if (!self::isSulfuras($item)) {
 						$item->setQuality($item->getQuality() - 1);
 					}
 				}
 			} else {
 				if ($item->getQuality() < 50) {
 					$item->setQuality($item->getQuality() + 1);
-					if ("Backstage passes to a TAFKAL80ETC concert" == $item->getName()) {
+					if (self::isBackstagePass($item)) {
 						if ($item->getSellIn() < 11) {
 							if ($item->getQuality() < 50) {
 								$item->setQuality($item->getQuality() + 1);
@@ -33,15 +51,15 @@ class GildedRose {
 				}
 			}
 
-			if ("Sulfuras, Hand of Ragnaros" != $item->getName()) {
+			if (!self::isSulfuras($item)) {
 				$item->setSellIn($item->getSellIn() - 1);
 			}
 
 			if ($item->getSellIn() < 0) {
-				if ("Aged Brie" != $item->getName()) {
-					if ("Backstage passes to a TAFKAL80ETC concert" != $item->getName()) {
+				if (!self::isAgedBrie($item)) {
+					if (!self::isBackstagePass($item)) {
 						if ($item->getQuality() > 0) {
-							if ("Sulfuras, Hand of Ragnaros" != $item->getName()) {
+							if (!self::isSulfuras($item)) {
 								$item->setQuality($item->getQuality() - 1);
 							}
 						}
